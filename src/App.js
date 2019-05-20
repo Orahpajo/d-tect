1b35dff5c274626b1e2f9af1f91e43d9238c9d89
Marvin Schön <marvin.schoen89@gmail.com>
2019-05-20 23:50:53 +0200

    Operation GUI added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Navbar } from 'react-bootstrap';
+import Operation from './components/operation.component';
 
 function App() {
   return (
@@ -9,13 +10,14 @@ function App() {
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
       <div className="container">
         <Navbar bg='light' expand='lg'>
           <Link to='/' className='navbar-brand'>d-tect</Link>
         </Navbar>
       </div>
+      <Route path="/" exact component={Operation} />
     </Router>
   );
 }
--- src/components/operation.component.js
+++ src/components/operation.component.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
 
 export default class Operation extends Component {
-    render () {
-        <div className="container">
-            <h2>Hier wird die Einsatz ID eingegeben.</h2>
-        </div>
+    render() {
+        return (
+            <div style={{marginTop: 10}}>
+                <Container>
+                    <Form>
+                        <Form.Group controlId="formBasicOperationID">
+                            <Form.Label>Geben Sie die Messauftragsnummer ein</Form.Label>
+                            <Form.Control placeholder="Messauftragsnummer" />
+                        </Form.Group>
+                        <Button type="submit">
+                            Los!
+                        </Button>
+                    </Form>
+                </Container>
+            </div>
+        )
     }
 }
