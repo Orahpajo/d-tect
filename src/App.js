596162a22f43a7636f545698f4146ee3715916a9
Marvin Schön <marvin.schoen89@gmail.com>
2019-05-20 07:29:18 +0200

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <div className="container">
+        <Navbar bg='light' expand='lg'>
+          <Link to='/' className='navbar-brand'>d-tect</Link>
+        </Navbar>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/operation.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Operation extends Component {
+    render () {
+        <div className="container">
+            <h2>Hier wird die Einsatz ID eingegeben.</h2>
+        </div>
+    }
+}
